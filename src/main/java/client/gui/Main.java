aa5389eaf810abe4b7748a04f6c73b32793dcd6f
GrzeX - laptop <gregory.ratajczak@gmail.com>
2018-11-16 00:12:08 +0100

    add main for server and text for doc

--- src/main/java/client/gui/Main.java
+++ src/main/java/client/gui/Main.java
@@ -1,8 +1,11 @@
-package gui;
+package client.gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * main for client application - chat app on tcp/ip with javafx
+ */
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
---
+++ src/main/java/server/Main.java
@@ -0,0 +1,11 @@
+package server;
+
+/**
+ * main for server - chat app with tcp/ip, database - mysql
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
