2c6288c98090f27a57a5b729cc53ef7b3ea50d1d
GrzeX - laptop <gregory.ratajczak@gmail.com>
2018-11-18 11:10:20 +0100

    MainServer add socket and io streams

--- src/main/java/client/gui/MainClient.java
+++ src/main/java/client/gui/MainClient.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 /**
  * main for client application - chat app on tcp/ip with javafx
  */
-public class Main extends Application {
+public class MainClient extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
---
+++ src/main/java/server/MainServer.java
@@ -0,0 +1,22 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainServer {
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(6666);
+        Socket clientSocket = serverSocket.accept();
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+    }
+}
